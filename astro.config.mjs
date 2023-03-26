b9cfc1726d482d2ea9271ced2598e7a4e52e28c4
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-03-26 23:17:04 +0200

    Testing CI for deno build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: deno()
 });

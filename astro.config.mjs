23fdffa20f3161405b9125aae41bbdcc112a6b01
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-03-26 19:38:27 +0200

    using node ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });

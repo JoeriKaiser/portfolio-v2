56518d082002d0dab9a2ed33e2461581373d1d63
Joeri Kaiser <joeri.kaiser@medelse.com>
2023-04-01 15:05:16 +0200

    Configuartion files: eslint, prettier, astro, tsconfig, pnpm, packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
-
 import deno from "@astrojs/deno";
 
 // https://astro.build/config
